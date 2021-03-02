1709191f1dd542b69f5740c89deb0eac7481bf47
JabinGP <1748511181@qq.com>
2021-03-02 22:37:57 +0800

    refactor: go-template replace goquery

--- parser/tag.go
+++ parser/tag.go
@@ -3,15 +3,15 @@ package parser
 import (
 	"bytes"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"path/filepath"
-	"strings"
 
 	"github.com/JabinGP/mdout/log"
+	"github.com/JabinGP/mdout/model"
 	"github.com/JabinGP/mdout/requester"
 	"github.com/JabinGP/mdout/static"
 	"github.com/JabinGP/mdout/tool"
-	"github.com/PuerkitoBio/goquery"
 )
 
 // TagFileParser Parse tag file to tag bytes
@@ -37,12 +37,25 @@ type TagBytesParser struct {
 
 // Parse markdown to html
 func (t *TagBytesParser) Parse(req *requester.Request) error {
-	tagBytes := req.Data.([]byte)
+	// tagBytes := req.Data.([]byte)
 
 	if !tool.IsExists(filepath.FromSlash(static.ThemeFolderFullName + "/" + req.ThemeName)) {
 		return fmt.Errorf("无法找到名为 %s 的主题", req.ThemeName)
 	}
 	log.Debugln("开始生成html...")
+	htmlBytes, err := t.buildHTMLFromTemplate(req)
+	if err != nil {
+		log.Errorf("生成html失败: %v", err)
+		return err
+	}
+	log.Debugln("成功生成html")
+
+	req.Data = htmlBytes
+	req.InType = "html-bytes"
+	return nil
+}
+
+func (t *TagBytesParser) buildHTMLFromTemplate(req *requester.Request) ([]byte, error) {
 	// 获取资源文件夹路径
 	var themeDir = filepath.FromSlash(static.ThemeFolderFullName +
 		"/" + req.ThemeName)
@@ -50,55 +63,20 @@ func (t *TagBytesParser) Parse(req *requester.Request) error {
 	var indexHTMLFullName = filepath.FromSlash(themeDir +
 		"/index.html")
 
-	// 页面模板
-	indexHTMLBytes, err := ioutil.ReadFile(indexHTMLFullName)
+	// 读取模板
+	tmpl, err := template.ParseFiles(indexHTMLFullName)
 	if err != nil {
-		return err
+		log.Errorf("解析主题模板 %s 失败: %v", indexHTMLFullName, err)
+		return nil, err
 	}
 
-	// 获取主体html模板的Reader，用于goquery
-	indexHTMLReader := bytes.NewReader(indexHTMLBytes)
+	// 将数据放入模板
+	buf := new(bytes.Buffer)
 
-	// 获取HtmlDocument对象
-	indexHTMLDoc, err := goquery.NewDocumentFromReader(indexHTMLReader)
-	if err != nil {
-		return err
-	}
-
-	// 拼装页面
-	indexHTMLDoc.Find(".markdown-body").AppendHtml(string(tagBytes)) // 将markdown标签插入html
-
-	// 将link标签和script标签中的{{homePath}}和{{theme}}替换成为实际路径
-	indexHTMLDoc.Find("link").Each(func(i int, selection *goquery.Selection) {
-		linkHref, ok := selection.Attr("href") // 获取href属性
-		if ok {                                // 如果获取成功
-			// 替换
-			linkHref = strings.Replace(linkHref, `{{themePath}}`, filepath.ToSlash(themeDir), -1)
-			// 生效
-			selection.SetAttr("href", linkHref)
-		}
-	})
-	indexHTMLDoc.Find("script").Each(func(i int, selection *goquery.Selection) {
-		srcPath, ok := selection.Attr("src") // 获取src属性
-		if ok {                              // 如果获取成功
-			// 替换
-			srcPath = strings.Replace(srcPath, `{{themePath}}`, filepath.ToSlash(themeDir), -1)
-			// 生效
-			selection.SetAttr("src", srcPath)
-		}
+	tmpl.Execute(buf, model.TemplateData{
+		ThemePath:    template.URL(filepath.ToSlash(themeDir)),
+		MarkdownBody: template.HTML(req.Data.([]byte)),
 	})
 
-	// 获取拼接后的html字符串
-	assembledHTML, err := indexHTMLDoc.Html()
-	if err != nil {
-		return err
-	}
-
-	// 构建byte数组
-	htmlBytes := []byte(assembledHTML)
-	log.Debugln("成功生成html")
-
-	req.Data = htmlBytes
-	req.InType = "html-bytes"
-	return nil
+	return buf.Bytes(), nil
 }
